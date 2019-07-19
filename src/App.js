f73752664417aff1c130d79755b206b538a5d335
karmjeet Saroj <karmpro19@gmail.com>
2019-07-19 12:32:44 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import Painting from './Painting/Painting';
+
+
+
+class App extends React.Component {
+
+  state = {
+    paintings :[
+      {name:'ballista', category:'nature',Id:1,title:'nature elegance is a bliss',author:'Akshita'},
+      {name:'charista', category:'world',Id:2,title:'into the mystrious cave',author:'meethi'},
+      {name:'mirista', category:'death',Id:3,title:'face of the devil',author:'john'},
+      {name:'monalista', category:'human',Id:4,title:'find your expression',author:'Asta'},
+      {name:'choopista', category:'movie',Id:5,title:'dhoop chahiye isko',author:'krish'}
+    ]
+  };
+
+   deleteCurrentbox = (boxindex)=>{   // this is the current method in painting which removes the current box ,recieving index of the box as argument
+    const paintingreplica = [...this.state.paintings]; //we are duplicating the paintings because arrays and objects are pass by reference in javascript so this will create an copy of the object painting and by doing this we are avoiding direct update on the object
+     paintingreplica.splice(boxindex,1); //  by index we are removing the box from the array
+     this.setState({paintings:paintingreplica}); // finally updating the state with replica
+
+  }
+
+  nameChangeHandler = (event,id) =>{
+      const paintingreplica = [...this.state.paintings];
+      const currentpainting = paintingreplica.find(id);
+      console.log(currentpainting);
+  }
+
+  // this the render method of react component to display the jsx
+render() {
+
+let  paint = (
+   <div>
+    {this.state.paintings.map((painting,boxindex) => {
+     return  <Painting
+               name={painting.name}
+               category={painting.category}
+               key ={painting.Id}
+               title={painting.title}
+               author={painting.author}
+               close = {this.deleteCurrentbox.bind(this,boxindex)}
+               changed ={(event)=>this.nameChangeHandler(event,painting.Id)}  // as this is a fn ,so it will get the event first that's why event in function argument then passing the event and Id to the handler
+               />
+})}
+</div>    /// this is the enclosing tag which is needed to be included if you are generating multiple jsx code
+);
+  return (
+    <div className="App">
+      <header className="App-header">
+          {paint}
+      </header>
+
+    </div>
+  );
+}
+
+}
+
+export default App;
---
+++ src/Painting/Painting.js
@@ -0,0 +1,27 @@
+import React from  'react';
+import './../App.css';
+import './paintngs.css';
+
+
+const painting  = (props) => {    // this is functional component of react where we are creating a function which is not extending the "component class and returning the JSX"
+  return (
+    <div>
+      <div className="blue painting-box">
+       <button className="close-button" onClick={props.close}>X</button>
+       <div className="container-box">
+        <div className="title">{props.title}</div>
+        <div className="details">
+
+        <span>{props.name} </span>
+        <span>{props.category} </span>
+        <span>{props.author} </span>
+        <input type="text" onChange={props.change} value={props.name}/>
+        </div>
+       </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default painting;
