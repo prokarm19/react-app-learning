2851a17c4f313cb8cea39cfb72fc100c80cfc844
karmjeet <karmpro19@gmail.com>
2019-09-05 14:29:14 +0530

    converted stateless component to statefull component[Persons.js]

--- src/components/Paintings/Painting/Painting.js
+++ src/components/Paintings/Painting/Painting.js
@@ -2,27 +2,66 @@ import React from 'react';
 import '../Paintings';
 import './paintngs.css';
 
+class Painting extends React.Component{
+  constructor(props){
+  super(props)
+  console.log(`[Painting.js] inside constructor`,props);;
+  }
+  componentWillUnmount(){
+    console.log(`[Painting.js] inside componentWillMount`);
+  }
+  componentDidMount(){
+    console.log(`[Painting.js] inside componentDidMount`);
+  }
 
-const painting  = (props) => {    // this is functional component of react where we are creating a function which is not extending the "component class and returning the JSX"
-  return (
-    <div>
-      <div className="blue painting-box">
-       <img src={props.pic} alt={props.title}/>
-       <button className="close-button" onClick={props.close}>X</button>
-       <div className="container-box">
-        <div className="title">{props.title}</div>
-        <div className="details">
+  render(){
+  console.log(`[Painting.js] inside render`);
 
-        <span>{props.name} </span>
-        <span>{props.category} </span>
-        <span>{props.author} </span>
-        <input type="text" onChange={props.changed}/>
+    return (
+      <div>
+        <div className="blue painting-box">
+         <img src={this.props.pic} alt={this.props.title}/>
+         <button className="close-button" onClick={this.props.close}>X</button>
+         <div className="container-box">
+          <div className="title">{this.props.title}</div>
+          <div className="details">
+
+          <span>{this.props.name} </span>
+          <span>{this.props.category} </span>
+          <span>{this.props.author} </span>
+          <input type="text" onChange={this.props.changed}/>
+          </div>
+         </div>
         </div>
-       </div>
       </div>
-    </div>
-  );
-}
+    );
+  }// end render
+} // end class
 
 
-export default painting;
+
+// this is the functional approach of the components
+// ***************************************************************
+// const painting  = (props) => {    // this is functional component of react where we are creating a function which is not extending the "component class and returning the JSX"
+//   return (
+//     <div>
+//       <div className="blue painting-box">
+//        <img src={props.pic} alt={props.title}/>
+//        <button className="close-button" onClick={props.close}>X</button>
+//        <div className="container-box">
+//         <div className="title">{props.title}</div>
+//         <div className="details">
+//
+//         <span>{props.name} </span>
+//         <span>{props.category} </span>
+//         <span>{props.author} </span>
+//         <input type="text" onChange={props.changed}/>
+//         </div>
+//        </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+export default Painting;
--- src/components/Paintings/Paintings.js
+++ src/components/Paintings/Paintings.js
@@ -1,20 +1,45 @@
-import React from 'react';
+ import React ,{Component}from 'react';
 import Painting from   "./Painting/Painting";
 
-
-let paintings = (props)=> {
-  return props.paint.map((painting,boxindex) => {
-   return  <Painting
-             name={painting.name}
-             category={painting.category}
-             key ={painting.id}
-             title={painting.title}
-             author={painting.author}
-             close = {props.deleted.bind(this,boxindex)}
-             changed ={(event) => props.changed(event , painting.id)}  // as this is a fn ,so it will get the event first that's why event in function argument then passing the event and id to the handler
-             pic = {painting.img}
-             />
-})
+class Paintings extends Component{
+  constructor(props){
+  super(props)
+  console.log(`[Paintings.js] inside constructor ${props}`);;
+  }
+  componentWillUnmount(){
+    console.log(`[Paintings.js] inside componentWillMount`);
+  }
+  componentDidMount(){
+    console.log(`[Paintings.js] inside componentDidMount`);
+  }
+  render (){
+     return this.props.paint.map((painting,boxindex) => {
+        return  <Painting
+                  name={painting.name}
+                  category={painting.category}
+                  key ={painting.id}
+                  title={painting.title}
+                  author={painting.author}
+                  close = {this.props.deleted.bind(this,boxindex)}
+                  changed ={(event) => this.props.changed(event , painting.id)}  // as this is a fn ,so it will get the event first that's why event in function argument then passing the event and id to the handler
+                  pic = {painting.img}
+                  />
+     })
+  }
 }
+// let paintings = (props)=> {
+//   return props.paint.map((painting,boxindex) => {
+//    return  <Painting
+//              name={painting.name}
+//              category={painting.category}
+//              key ={painting.id}
+//              title={painting.title}
+//              author={painting.author}
+//              close = {props.deleted.bind(this,boxindex)}
+//              changed ={(event) => props.changed(event , painting.id)}  // as this is a fn ,so it will get the event first that's why event in function argument then passing the event and id to the handler
+//              pic = {painting.img}
+//              />
+// })
+// }
 
-export default paintings;
+export default Paintings;
